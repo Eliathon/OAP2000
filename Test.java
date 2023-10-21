8820813b2b6dbc86b7af477ba0a4a64fe33f7e92
Johnny Strømø <113671758+JohnnyBravo1983@users.noreply.github.com>
2023-10-21 10:20:22 +0200

    Important update
    setup for elchapo

---
+++ Test.java
@@ -0,0 +1,19 @@
+public class Test {
+
+
+
+public static void main (String [] args)
+
+
+
+//New DrugLord Object 
+DrugLord ElChapo = new DrugLord(Elcapitan ,1300 , 15000, True, Sinaloa,Sweedish);
+
+
+// New CocainDealers Object
+CocainDealers VinnieJones= new CocainDealers (Vinni, ElChapo, 2400, 250, True , Sinaloa)
+
+
+ElChapo.DrugLordInfo();
+System.out.println("Vinnie Jones works for me and he sells a monthly volum of " +getVinniJones.monthlyVolum ());
+}
