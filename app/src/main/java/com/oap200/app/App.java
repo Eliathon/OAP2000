705036f9b61effa3ec4d8b89390d5bf5643b5b88
KristianThue <146175490+KristianThue@users.noreply.github.com>
2023-10-30 14:07:41 +0100

    fjernet test

--- app/src/main/java/com/oap200/app/App.java
+++ app/src/main/java/com/oap200/app/App.java
@@ -13,7 +13,7 @@ import java.sql.Statement;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-//test2, Kristian
+
 public class App {
 	public static void main(String[] args) {
 		List<String> emails = new ArrayList<>();
