c3766eec29b3cfb92be7d5f2d0ce4a865de2b72a
KristianThue <146175490+KristianThue@users.noreply.github.com>
2023-10-31 14:15:55 +0100

    Many fixes

--- app/src/main/java/com/oap200/app/views/EmployeeManagement.java
+++ app/src/main/java/com/oap200/app/views/EmployeeManagement.java
@@ -5,15 +5,15 @@ package com.oap200.app.views;
 import javax.swing.*;
 import java.awt.*;
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.Vector;
 import javax.swing.table.DefaultTableModel;
-
 
 public class EmployeeManagement {
     private JFrame frame;
     private Connection connection;
     private JTable table;
-    private JTextField nameField, ageField, searchField;
+    private JTextField employeeNumberSearchField, searchField, firstNameField, lastNameField, emailField, jobTitleField;
     private JButton addButton, updateButton, deleteButton, refreshButton, searchButton;
 
     public EmployeeManagement() {
@@ -22,72 +22,92 @@ public class EmployeeManagement {
     }
 
     private void connectDatabase() {
-        String url = "jdbc:mysql://localhost:3306/classicmodels?useSSL=false&serverTimezone=UTC";
-        String user = "root"; 
-        String password = ""; 
-        
+        String url = "jdbc:mysql://localhost:3306/classicmodels";
+        String user = "root";
+        String password = "";
+
         try {
             // Load MySQL JDBC Driver
             Class.forName("com.mysql.cj.jdbc.Driver");
-            
             connection = DriverManager.getConnection(url, user, password);
-            PreparedStatement ps = connection.prepareStatement("CREATE TABLE IF NOT EXISTS employees(id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255), age INT)");
-            ps.execute();
         } catch (SQLException | ClassNotFoundException e) {
             e.printStackTrace();
         }
     }
-    
-    
 
     private void createUI() {
         frame = new JFrame("Employee Management");
-        frame.setSize(600, 400);
+        frame.setSize(1100, 600); // Adjusted width for better layout
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new BorderLayout());
-
-        JPanel inputPanel = new JPanel(new FlowLayout());
-        searchField = new JTextField(10);
-        searchButton = new JButton("Search");
-        nameField = new JTextField(10);
-        ageField = new JTextField(3);
+    
+        JPanel inputPanel = new JPanel(new BorderLayout());
+        
+        // Separating the search panel
+        JPanel searchPanel = new JPanel(new FlowLayout());
+        JTextField employeeNumberSearchField = new JTextField(10);
+        JButton employeeNumberSearchButton = new JButton("Search by Employee Number");
+        searchField = new JTextField(15);
+        searchButton = new JButton("Search by Last Name");
+        
+        searchPanel.add(new JLabel("Employee Number:"));
+        searchPanel.add(employeeNumberSearchField);
+        searchPanel.add(employeeNumberSearchButton);
+        searchPanel.add(new JLabel("Last Name:"));
+        searchPanel.add(searchField);
+        searchPanel.add(searchButton);
+    
+        // Creating the add/update employee panel
+        JPanel employeeInputPanel = new JPanel(new GridLayout(2, 7)); 
+    
+        firstNameField = new JTextField(10);
+        lastNameField = new JTextField(10);
+        emailField = new JTextField(20);
+        jobTitleField = new JTextField(15);
         addButton = new JButton("Add");
         updateButton = new JButton("Update");
         deleteButton = new JButton("Delete");
         refreshButton = new JButton("Refresh");
-
-        inputPanel.add(new JLabel("Search by Name:"));
-        inputPanel.add(searchField);
-        inputPanel.add(searchButton);
-        inputPanel.add(new JLabel("Name:"));
-        inputPanel.add(nameField);
-        inputPanel.add(new JLabel("Age:"));
-        inputPanel.add(ageField);
-        inputPanel.add(addButton);
-        inputPanel.add(updateButton);
-        inputPanel.add(deleteButton);
-        inputPanel.add(refreshButton);
-
+    
+        employeeInputPanel.add(new JLabel("First Name:"));
+        employeeInputPanel.add(firstNameField);
+        employeeInputPanel.add(new JLabel("Last Name:"));
+        employeeInputPanel.add(lastNameField);
+        employeeInputPanel.add(new JLabel("Email:"));
+        employeeInputPanel.add(emailField);
+        employeeInputPanel.add(new JLabel("Job Title:"));
+        employeeInputPanel.add(jobTitleField);
+        employeeInputPanel.add(addButton);
+        employeeInputPanel.add(updateButton);
+        employeeInputPanel.add(deleteButton);
+        employeeInputPanel.add(refreshButton);
+    
+        // Add panels to the main input panel
+        inputPanel.add(searchPanel, BorderLayout.NORTH);
+        inputPanel.add(employeeInputPanel, BorderLayout.CENTER);
+    
         table = new JTable();
         refreshTable();
-
+    
         JScrollPane scrollPane = new JScrollPane(table);
-
+    
         frame.add(inputPanel, BorderLayout.NORTH);
         frame.add(scrollPane, BorderLayout.CENTER);
-
-        searchButton.addActionListener(e -> searchEmployee());
+    
+        // Adjusted the search button's action to differentiate between employee number and last name search.
+        employeeNumberSearchButton.addActionListener(e -> searchEmployeeByNumber(employeeNumberSearchField.getText()));
+        searchButton.addActionListener(e -> searchEmployeeByLastName(searchField.getText()));
         addButton.addActionListener(e -> addEmployee());
         updateButton.addActionListener(e -> updateEmployee());
         deleteButton.addActionListener(e -> deleteEmployee());
         refreshButton.addActionListener(e -> refreshTable());
-
+    
         frame.setVisible(true);
     }
 
     private void refreshTable() {
         try {
-            PreparedStatement ps = connection.prepareStatement("SELECT * FROM employees");
+            PreparedStatement ps = connection.prepareStatement("SELECT employeeNumber, firstName, lastName, email, jobTitle FROM employees");
             ResultSet rs = ps.executeQuery();
             populateTableFromResultSet(rs);
         } catch (SQLException e) {
@@ -95,10 +115,21 @@ public class EmployeeManagement {
         }
     }
 
-    private void searchEmployee() {
+    private void searchEmployeeByNumber(String empNumber) {
         try {
-            PreparedStatement ps = connection.prepareStatement("SELECT * FROM employees WHERE name LIKE ?");
-            ps.setString(1, "%" + searchField.getText() + "%");
+            PreparedStatement ps = connection.prepareStatement("SELECT employeeNumber, firstName, lastName, email, jobTitle FROM employees WHERE employeeNumber LIKE ?");
+            ps.setString(1, "%" + empNumber + "%");
+            ResultSet rs = ps.executeQuery();
+            populateTableFromResultSet(rs);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    private void searchEmployeeByLastName(String lastName) {
+        try {
+            PreparedStatement ps = connection.prepareStatement("SELECT employeeNumber, firstName, lastName, email, jobTitle FROM employees WHERE lastName LIKE ?");
+            ps.setString(1, "%" + lastName + "%");
             ResultSet rs = ps.executeQuery();
             populateTableFromResultSet(rs);
         } catch (SQLException e) {
@@ -108,55 +139,40 @@ public class EmployeeManagement {
 
     private void populateTableFromResultSet(ResultSet rs) throws SQLException {
         Vector<String> columnNames = new Vector<>();
-        columnNames.add("ID");
-        columnNames.add("Name");
-        columnNames.add("Age");
+        columnNames.add("Employee Number");
+        columnNames.add("First Name");
+        columnNames.add("Last Name");
+        columnNames.add("Email");
+        columnNames.add("Job Title");
+
         Vector<Vector<Object>> data = new Vector<>();
         while (rs.next()) {
             Vector<Object> row = new Vector<>();
-            row.add(rs.getInt("id"));
-            row.add(rs.getString("name"));
-            row.add(rs.getInt("age"));
+            row.add(rs.getInt("employeeNumber"));
+            row.add(rs.getString("firstName"));
+            row.add(rs.getString("lastName"));
+            row.add(rs.getString("email"));
+            row.add(rs.getString("jobTitle"));
             data.add(row);
         }
-        table.setModel(new DefaultTableModel(data, columnNames));
 
+        table.setModel(new DefaultTableModel(data, columnNames));
     }
 
     private void addEmployee() {
-        try {
-            PreparedStatement ps = connection.prepareStatement("INSERT INTO employees (name, age) VALUES (?, ?)");
-            ps.setString(1, nameField.getText());
-            ps.setInt(2, Integer.parseInt(ageField.getText()));
-            ps.execute();
-            refreshTable();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        JOptionPane.showMessageDialog(frame, "Adding employees functionality is not implemented yet.");
     }
 
     private void updateEmployee() {
-        if (table.getSelectedRow() != -1) {
-            int id = (int) table.getValueAt(table.getSelectedRow(), 0);
-            try {
-                PreparedStatement ps = connection.prepareStatement("UPDATE employees SET name = ?, age = ? WHERE id = ?");
-                ps.setString(1, nameField.getText());
-                ps.setInt(2, Integer.parseInt(ageField.getText()));
-                ps.setInt(3, id);
-                ps.execute();
-                refreshTable();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
+        JOptionPane.showMessageDialog(frame, "Updating employees functionality is not implemented yet.");
     }
 
     private void deleteEmployee() {
         if (table.getSelectedRow() != -1) {
-            int id = (int) table.getValueAt(table.getSelectedRow(), 0);
+            int employeeNumber = (int) table.getValueAt(table.getSelectedRow(), 0);
             try {
-                PreparedStatement ps = connection.prepareStatement("DELETE FROM employees WHERE id = ?");
-                ps.setInt(1, id);
+                PreparedStatement ps = connection.prepareStatement("DELETE FROM employees WHERE employeeNumber = ?");
+                ps.setInt(1, employeeNumber);
                 ps.execute();
                 refreshTable();
             } catch (SQLException e) {
@@ -165,11 +181,7 @@ public class EmployeeManagement {
         }
     }
 
-    public void start() {
-        SwingUtilities.invokeLater(() -> new EmployeeManagement());
-    }
-
     public static void main(String[] args) {
-        new EmployeeManagement().start();
+        SwingUtilities.invokeLater(() -> new EmployeeManagement());
     }
 }
