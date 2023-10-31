26835e4fb74a8f210a8c866333e0575bc2d1f9fb
Pat-Pas <Pastor301299@gmail.com>
2023-11-01 00:33:04 +0100

    Added a connection to the classicmodels databas in sql. Aswell as an SQL statement to execute a SQL query on the database. The result will use a while loop to collect product names that match the search query. And at the end close the database connection to release resources and cleanup.

--- app/src/main/java/com/oap200/app/views/OrderManagementPanel.java
+++ app/src/main/java/com/oap200/app/views/OrderManagementPanel.java
@@ -4,7 +4,6 @@ import javax.swing.*;
 
 import java.awt.BorderLayout;
 import java.util.List;
-// TEST
 public class OrderManagementPanel extends JPanel {
 
     private JTextArea textArea = new JTextArea();
