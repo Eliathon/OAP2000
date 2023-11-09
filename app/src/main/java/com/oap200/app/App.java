8e450f44c12b9ac3a1e05242b8b86bdf1e257507
Eliathon <novi.sindre@gmail.com>
2023-11-09 19:19:12 +0100

    Edit to App.java

--- app/src/main/java/com/oap200/app/App.java
+++ app/src/main/java/com/oap200/app/App.java
@@ -1,42 +1,15 @@
-// Created by Sindre
 package com.oap200.app;
 
-import com.oap200.app.utils.DbConnect;
-import com.oap200.app.views.MainFrame;
-
+import com.oap200.app.views.LoginPanel;
 import javax.swing.SwingUtilities;
-
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
 
 public class App {
 	public static void main(String[] args) {
-		List<String> emails = new ArrayList<>();
-
-		try {
-			DbConnect db = new DbConnect();
-			Connection myConnection = db.getConnection();
-			Statement myStmt = myConnection.createStatement();
-			ResultSet myRs = myStmt.executeQuery("select * from employees");
-
-			// Fetch emails
-			while (myRs.next()) {
-				emails.add(myRs.getString("email"));
+		// Launch the LoginPanel on the Event Dispatch Thread
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				new LoginPanel().setVisible(true);
 			}
-
-		} catch (SQLException | ClassNotFoundException ex) {
-			ex.printStackTrace();
-		}
-
-		// Display the emails in the Swing frame
-		SwingUtilities.invokeLater(() -> {
-			MainFrame frame = new MainFrame();
-			frame.displayEmails(emails);
-			frame.setVisible(true);
 		});
 	}
 }
