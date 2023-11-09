6e0b3c0996d25e3ec9f50aa46d31bab182a6c88d
Eliathon <novi.sindre@gmail.com>
2023-11-09 18:54:53 +0100

    merge

--- app/src/main/java/com/oap200/app/views/PaymentManagement.java
+++ app/src/main/java/com/oap200/app/views/PaymentManagement.java
@@ -1,47 +1,91 @@
 package com.oap200.app.views;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.*;
-import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.math.BigDecimal;
 
-public class PaymentManagement extends JPanel {
+public class PaymentManagement {
+
     private JFrame frame;
     private JPanel panel;
     private JButton viewButton;
     private JButton addButton;
     private JButton deleteButton;
+    private JButton updateButton;
     private JTextField customerNumberField;
     private JTextField checkNumberField;
     private JTextField paymentDateField;
     private JTextField amountField;
+    private JTextField paymentsField;
+    private JTable resultTable;
+    private JPanel mainPanel;
+
     private JTextArea textArea = new JTextArea();
+
+    private JScrollPane textAreaScrollPane;
+
+    private JTextArea resultMessageArea;
+    private JScrollPane messageScrollPane;
 
     private Connection connection;
 
-    public PaymentManagement() {
-        initializeUI();
-        connectToDatabase();
-    }
+    // ... Rest of your code
 
-    private void initializeUI() {
+    public void start() {
+
         frame = new JFrame("Payment Management");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(1100, 600); // Økt høyden for å plassere flere komponenter
+        frame.setLayout(new BorderLayout());
 
-        panel = new JPanel();
-        panel.setLayout(new FlowLayout());
+        mainPanel = new JPanel(new BorderLayout());
 
-        customerNumberField = new JTextField(10);
-        checkNumberField = new JTextField(10);
-        paymentDateField = new JTextField(10);
-        amountField = new JTextField(10);
+        textAreaScrollPane = new JScrollPane(textArea);
+        textAreaScrollPane.setPreferredSize(new Dimension(800, 100));
+
+        resultTable = new JTable();
+        JScrollPane tableScrollPane = new JScrollPane(resultTable);
+        tableScrollPane.setPreferredSize(new Dimension(1900, 600));
+
+        resultMessageArea = new JTextArea();
+        resultMessageArea.setEditable(false);
+        messageScrollPane = new JScrollPane(resultMessageArea);
+        messageScrollPane.setPreferredSize(new Dimension(800, 100));
+
+        JPanel topPanel = new JPanel(new BorderLayout());
+        topPanel.setLayout(new FlowLayout());
+
+        JPanel centerPanel = new JPanel(new BorderLayout());
+        centerPanel.setLayout(new FlowLayout());
+
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new FlowLayout());
+
+        JPanel bottomPanel = new JPanel(new BorderLayout());
+        bottomPanel.setLayout(new FlowLayout());
+
+        JLabel customerNumberLabel = new JLabel("Customer Number Field:");
+        customerNumberField = new JTextField();
+
+        JLabel checkNumberLabel = new JLabel("Check Number Field:");
+        checkNumberField = new JTextField();
+
+        JLabel paymentDateLabel = new JLabel("Payment Date Field:");
+        paymentDateField = new JTextField();
+
+        JLabel amountLabel = new JLabel("Amount Field:");
+        amountField = new JTextField();
+
+        JLabel paymentsLabel = new JLabel("Payments Field:");
+        paymentsField = new JTextField();
 
         viewButton = new JButton("View Payments");
-        addButton = new JButton("Add Payment");
-        deleteButton = new JButton("Delete Payment");
-
         viewButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -49,147 +93,236 @@ public class PaymentManagement extends JPanel {
             }
         });
 
+        addButton = new JButton("Add Payments");
         addButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                addPayment();
+                addPayments();
             }
         });
 
+        deleteButton = new JButton("Delete Payments");
         deleteButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                deletePayment();
+                deletePayments();
             }
         });
 
-        panel.add(new JLabel("Customer Number:"));
-        panel.add(customerNumberField);
-        panel.add(new JLabel("Check Number:"));
-        panel.add(checkNumberField);
-        panel.add(new JLabel("Payment Date (yyyy-MM-dd):"));
-        panel.add(paymentDateField);
-        panel.add(new JLabel("Amount:"));
-        panel.add(amountField);
-        panel.add(viewButton);
-        panel.add(addButton);
-        panel.add(deleteButton);
+        updateButton = new JButton("Update Payments");
+        updateButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                updatePayments();
+            }
+        });
+        buttonPanel.add(updateButton);
 
-        frame.add(panel, BorderLayout.NORTH);
-        frame.add(new JScrollPane(textArea), BorderLayout.CENTER);
+        topPanel.add(viewButton, BorderLayout.NORTH);
+        topPanel.add(addButton, BorderLayout.NORTH);
+        topPanel.add(deleteButton, BorderLayout.NORTH);
+        topPanel.add(updateButton, BorderLayout.NORTH);
 
-        frame.setSize(600, 400);
+        centerPanel.add(resultMessageArea, BorderLayout.CENTER);
+        bottomPanel.add(resultTable, BorderLayout.SOUTH);
+        centerPanel.add(textAreaScrollPane, BorderLayout.CENTER);
+
+        mainPanel.add(customerNumberLabel);
+        mainPanel.add(customerNumberField);
+        mainPanel.add(checkNumberLabel);
+        mainPanel.add(checkNumberField);
+        mainPanel.add(paymentDateLabel);
+        mainPanel.add(paymentDateField);
+        mainPanel.add(amountLabel);
+        mainPanel.add(amountField);
+
+        frame.add(topPanel, BorderLayout.NORTH);
+        frame.add(centerPanel, BorderLayout.CENTER);
+        frame.add(bottomPanel, BorderLayout.SOUTH);
+
         frame.setVisible(true);
-    }
 
-    private void connectToDatabase() {
-        String url = "jdbc:mysql://localhost:3306/classicmodels";
-        String user = "root";
-        String password = "";
-
-        try {
-            connection = DriverManager.getConnection(url, user, password);
-        } catch (SQLException e) {
-            e.printStackTrace();
-            JOptionPane.showMessageDialog(null, "Failed to connect to the database.");
-        }
     }
 
     private void viewPayments() {
-        String query = "SELECT * FROM payments";
+        String searchQuery = paymentsField.getText();
+
         try {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/classicmodels", "root",
+                    "");
             Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery(query);
+            ResultSet resultSet = statement
+                    .executeQuery("SELECT * FROM payments WHERE checkNumber LIKE '%" + searchQuery + "%'");
 
-            textArea.setText(""); // Clear the text area
+            // Opprett et dataobjekt for tabellen
+            DefaultTableModel tableModel = new DefaultTableModel();
+            tableModel.addColumn("Customer Number");
+            tableModel.addColumn("Check Number");
+            tableModel.addColumn("Payment Date");
+            tableModel.addColumn("Amount");
 
+            // Fyll tabellen med data fra resultatsettet
             while (resultSet.next()) {
-                int customerNumber = resultSet.getInt("customerNumber");
+                String customerNumber = resultSet.getString("customerNumber");
                 String checkNumber = resultSet.getString("checkNumber");
-                Date paymentDate = resultSet.getDate("paymentDate");
-                double amount = resultSet.getDouble("amount");
+                String paymentDate = resultSet.getString("paymentDate");
+                String amount = resultSet.getString("amount");
 
-                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-                String paymentDateStr = dateFormat.format(paymentDate);
-
-                String paymentInfo = String.format(
-                        "Customer Number: %d, Check Number: %s, Payment Date: %s, Amount: %.2f\n",
-                        customerNumber, checkNumber, paymentDateStr, amount);
-
-                textArea.append(paymentInfo);
+                tableModel.addRow(new Object[] { customerNumber, checkNumber, paymentDate, amount });
             }
-        } catch (SQLException e) {
-            e.printStackTrace();
-            JOptionPane.showMessageDialog(null, "Failed to retrieve payments.");
+
+            // Oppdater JTable med det nye datamodellen
+            resultTable.setModel(tableModel);
+
+            connection.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
         }
     }
 
-    private void addPayment() {
-        String insertQuery = "INSERT INTO payments (customerNumber, checkNumber, paymentDate, amount) VALUES (?, ?, ?, ?)";
+    // Insert code here
+
+    private void addPayments() {
+        String customerNumberInput = customerNumberField.getText();
+        String checkNumberInput = checkNumberField.getText();
+        String paymentDateInput = paymentDateField.getText();
+        String amountInput = amountField.getText();
 
         try {
-            PreparedStatement preparedStatement = connection.prepareStatement(insertQuery);
-            int customerNumber = Integer.parseInt(customerNumberField.getText());
-            String checkNumber = checkNumberField.getText();
-            Date paymentDate = Date.valueOf(paymentDateField.getText());
-            double amount = Double.parseDouble(amountField.getText());
+            int customerNumber = Integer.parseInt(customerNumberInput);
+            String checkNumber = checkNumberInput;
+            // Parse the date string to a Date object if it's in the correct format
+            java.sql.Date paymentDate = java.sql.Date.valueOf(paymentDateInput);
+            BigDecimal amount = new BigDecimal(amountInput);
 
+            if (customerNumber > 0 && !checkNumber.isEmpty() && paymentDate != null
+                    && amount.compareTo(BigDecimal.ZERO) > 0) {
+                Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/classicmodels", "root",
+                        "");
+                PreparedStatement preparedStatement = connection.prepareStatement(
+                        "INSERT INTO payments (customerNumber, checkNumber, paymentDate, amount) VALUES (?, ?, ?, ?)");
+                preparedStatement.setInt(1, customerNumber);
+                preparedStatement.setString(2, checkNumber);
+                preparedStatement.setDate(3, paymentDate);
+                preparedStatement.setBigDecimal(4, amount);
+                preparedStatement.executeUpdate();
+
+                resultMessageArea.setText("Payment added successfully.");
+                connection.close();
+            } else {
+                resultMessageArea.setText("Invalid input data. Please check the values.");
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            resultMessageArea.setText("Failed to add payment.");
+        } catch (Exception ex) {
+            resultMessageArea.setText("Invalid input data. Please check the values.");
+        }
+    }
+
+    private void deletePayments() {
+        String customerNumberInput = customerNumberField.getText();
+        String checkNumberInput = checkNumberField.getText();
+        String paymentDateInput = paymentDateField.getText();
+        String amountInput = amountField.getText();
+
+        try {
+            int customerNumber = Integer.parseInt(customerNumberInput);
+            String checkNumber = checkNumberInput;
+            java.sql.Date paymentDate = java.sql.Date.valueOf(paymentDateInput);
+            BigDecimal amount = new BigDecimal(amountInput);
+
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/classicmodels", "root",
+                    "");
+            PreparedStatement preparedStatement = connection.prepareStatement(
+                    "DELETE FROM payments WHERE customerNumber = ? AND checkNumber = ? AND paymentDate = ? AND amount = ?");
             preparedStatement.setInt(1, customerNumber);
             preparedStatement.setString(2, checkNumber);
             preparedStatement.setDate(3, paymentDate);
-            preparedStatement.setDouble(4, amount);
+            preparedStatement.setBigDecimal(4, amount);
+            int rowsDeleted = preparedStatement.executeUpdate();
 
-            int rowsAffected = preparedStatement.executeUpdate();
-            if (rowsAffected > 0) {
-                JOptionPane.showMessageDialog(null, "Payment added successfully.");
+            if (rowsDeleted > 0) {
+                resultMessageArea.setText("Payment deleted successfully.");
             } else {
-                JOptionPane.showMessageDialog(null, "Failed to add payment.");
+                resultMessageArea.setText("No matching payment found to delete.");
             }
 
-            // Clear the input fields
-            customerNumberField.setText("");
-            checkNumberField.setText("");
-            paymentDateField.setText("");
-            amountField.setText("");
-        } catch (SQLException | NumberFormatException e) {
-            e.printStackTrace();
-            JOptionPane.showMessageDialog(null, "Failed to add payment.");
+            connection.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            resultMessageArea.setText("Failed to delete payment.");
+        } catch (Exception ex) {
+            resultMessageArea.setText("Invalid input data. Please check the values.");
         }
     }
 
-    private void deletePayment() {
-        String deleteQuery = "DELETE FROM payments WHERE customerNumber = ? AND checkNumber = ?";
+    private void updatePayments() {
+        String customerNumberInput = customerNumberField.getText();
+        String checkNumberInput = checkNumberField.getText();
+        String paymentDateInput = paymentDateField.getText();
+        String amountInput = amountField.getText();
+
         try {
-            PreparedStatement preparedStatement = connection.prepareStatement(deleteQuery);
-            int customerNumber = Integer.parseInt(customerNumberField.getText());
-            String checkNumber = checkNumberField.getText();
+            int customerNumber = Integer.parseInt(customerNumberInput);
+            String checkNumber = checkNumberInput;
+            java.sql.Date paymentDate = java.sql.Date.valueOf(paymentDateInput);
+            BigDecimal amount = new BigDecimal(amountInput);
 
-            preparedStatement.setInt(1, customerNumber);
-            preparedStatement.setString(2, checkNumber);
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/classicmodels", "root",
+                    "");
+            PreparedStatement preparedStatement = null;
+            int rowsUpdated = 0;
 
-            int rowsAffected = preparedStatement.executeUpdate();
-            if (rowsAffected > 0) {
-                JOptionPane.showMessageDialog(null, "Payment deleted successfully.");
-            } else {
-                JOptionPane.showMessageDialog(null, "No matching payment found to delete.");
+            if (!customerNumberInput.isEmpty()) {
+                preparedStatement = connection.prepareStatement(
+                        "UPDATE payments SET customerNumber = ? WHERE customerNumber = ?");
+                preparedStatement.setInt(1, customerNumber);
+                preparedStatement.setInt(2, Integer.parseInt(customerNumberInput));
+                rowsUpdated += preparedStatement.executeUpdate();
             }
 
-            // Clear the input fields
-            customerNumberField.setText("");
-            checkNumberField.setText("");
-            paymentDateField.setText("");
-            amountField.setText("");
-        } catch (SQLException | NumberFormatException e) {
-            e.printStackTrace();
-            JOptionPane.showMessageDialog(null, "Failed to delete payment.");
+            if (!checkNumberInput.isEmpty()) {
+                preparedStatement = connection.prepareStatement(
+                        "UPDATE payments SET checkNumber = ? WHERE customerNumber = ?");
+                preparedStatement.setString(1, checkNumber);
+                preparedStatement.setInt(2, customerNumber);
+                rowsUpdated += preparedStatement.executeUpdate();
+            }
+
+            if (!paymentDateInput.isEmpty()) {
+                preparedStatement = connection.prepareStatement(
+                        "UPDATE payments SET paymentDate = ? WHERE customerNumber = ?");
+                preparedStatement.setDate(1, paymentDate);
+                preparedStatement.setInt(2, customerNumber);
+                rowsUpdated += preparedStatement.executeUpdate();
+            }
+
+            if (!amountInput.isEmpty()) {
+                preparedStatement = connection.prepareStatement(
+                        "UPDATE payments SET amount = ? WHERE customerNumber = ?");
+                preparedStatement.setBigDecimal(1, amount);
+                preparedStatement.setInt(2, customerNumber);
+                rowsUpdated += preparedStatement.executeUpdate();
+            }
+
+            if (rowsUpdated > 0) {
+                resultMessageArea.setText("Payment(s) updated successfully.");
+            } else {
+                resultMessageArea.setText("No matching payment found to update.");
+            }
+
+            connection.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            resultMessageArea.setText("Failed to update payment.");
+        } catch (NumberFormatException ex) {
+            resultMessageArea.setText("Invalid input data. Please check the values.");
         }
     }
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                new PaymentManagement();
-            }
-        });
+        PaymentManagement paymentManagement = new PaymentManagement();
+        paymentManagement.start();
     }
 }
