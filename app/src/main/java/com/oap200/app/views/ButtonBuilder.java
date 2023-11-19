d46586ba79f17523614d26979eeac3992160ef10
MirjaM <249142m@gmail.com>
2023-11-19 21:38:48 +0100

    tabbed panels moduls and updates in mainframe/buttonbuilder

---
+++ app/src/main/java/com/oap200/app/views/ButtonBuilder.java
@@ -0,0 +1,64 @@
+// Created by Sindre
+
+package com.oap200.app.utils;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.SwingConstants;
+
+public class ButtonBuilder {
+    public static JButton createStyledButton(String text, Runnable action) {
+        ImageIcon buttonIcon = new ImageIcon(ButtonBuilder.class.getResource("/imgs/production-icon-19.png"));
+        Image resizedImage = buttonIcon.getImage().getScaledInstance(30, 30, Image.SCALE_SMOOTH);
+        ImageIcon buttonIconResized = new ImageIcon(resizedImage);
+
+        JButton button = new JButton(text, buttonIconResized);
+
+        button.setForeground(Color.BLACK); // Set text color
+        button.setBackground(new Color(255, 255, 255)); // Set background color
+        button.setBorderPainted(true); // Remove border
+        button.setHorizontalAlignment(SwingConstants.LEFT); // Align text to the left of the icon
+        button.setPreferredSize(new Dimension(200, 25)); // Set the desired width and height
+
+        button.addActionListener(e -> {
+            if (action != null) {
+                action.run();
+            }
+        });
+        return button;
+    }
+  // BlueBackButton is created by Johnny
+    public static JButton createBlueBackButton(Runnable action) {
+        JButton backButton = new JButton("BACK");
+        backButton.setForeground(Color.WHITE); // Set text color to white
+        backButton.setBackground(Color.BLUE); // Set background color to blue
+        backButton.setBorderPainted(false); // Remove border
+        backButton.setPreferredSize(new Dimension(90, 20)); // Set a specific size
+        backButton.addActionListener(e -> {
+            if (action != null) {
+                action.run();
+            }
+        });
+        return backButton;
+    }
+// Red Logout Button is created by Johnny
+    public static JButton createRedLogoutButton(Runnable action) {
+        JButton backButton = new JButton("Logout");
+        backButton.setForeground(Color.WHITE); // Set text color to white
+        backButton.setBackground(Color.RED); // Set background color to red
+        backButton.setBorderPainted(false); // Remove border
+        backButton.setPreferredSize(new Dimension(100, 20)); // Set a specific size
+        backButton.addActionListener(e -> {
+            if (action != null) {
+                action.run();
+            }
+        });
+        return backButton;
+    }
+
+}
+
--- app/src/main/java/com/oap200/app/views/TabbedCustomerPanel.java
+++ app/src/main/java/com/oap200/app/views/TabbedCustomerPanel.java
@@ -2,79 +2,55 @@
 
 
 package com.oap200.app.tabbedPanels;
-
 import javax.swing.*;
 import java.awt.*;
-import com.oap200.app.utils.ButtonBuilder; // Import the ButtonBuilder class
+import com.oap200.app.utils.ButtonBuilder;
 
-public class TabbedCustomerPanel extends JFrame {
-public TabbedCustomerPanel() {
-// Create the JTabbedPane
-JTabbedPane tabbedPane = new JTabbedPane();
+public class TabbedCustomerPanel extends JPanel {
 
-// Create the first tab.
-JPanel panel1 = new JPanel();
-// Add components to panel1...
-tabbedPane.addTab("View Customers", null, panel1, "Click to view");
+    public TabbedCustomerPanel() {
+        setLayout(new BorderLayout());
 
-// Create the second tab.
-JPanel panel2 = new JPanel();
-// Add components to panel2...
-tabbedPane.addTab("Add Customer", null, panel2, "Click to add ");
+        JTabbedPane tabbedPane = new JTabbedPane();
 
-// Create the third tab.
-JPanel panel3 = new JPanel();
-// Add components to panel3...
-tabbedPane.addTab("Update Customer", null, panel3, "Click to update ");
+        // Tab for Viewing Customers
+        JPanel viewCustomersPanel = new JPanel();
+        // Add components to viewCustomersPanel...
+        tabbedPane.addTab("View Customers", null, viewCustomersPanel, "Click to view");
 
-// Create the fourth tab.
-JPanel panel4 = new JPanel();
-// Add components to panel4...
-tabbedPane.addTab("Delete Customer", null, panel4, "Click to delete ");
+        // Tab for Adding Customers
+        JPanel addCustomersPanel = new JPanel();
+        // Add components to addCustomersPanel...
+        tabbedPane.addTab("Add Customer", null, addCustomersPanel, "Click to add");
 
-// Set up the layout for the frame
-setLayout(new BorderLayout());
-JPanel topPanel = new JPanel(new BorderLayout());
+        // Tab for Updating Customers
+        JPanel updateCustomersPanel = new JPanel();
+        // Add components to updateCustomersPanel...
+        tabbedPane.addTab("Update Customer", null, updateCustomersPanel, "Click to update");
 
-// Create and add the 'Back' button
-JButton backButton = ButtonBuilder.createBlueBackButton(() -> {
-// Define the action to be performed when the 'Back' button is clicked
-// Example: System.out.println("Back button clicked");
-});
-// Create and add the Logout button
-JButton logoutButton = ButtonBuilder.createRedLogoutButton(() -> {
-// Define the action to be performed when the 'Back' button is clicked
-// Example: System.out.println("Logout button clicked");      
- });
-        
-        
-// Create a panel to hold the back button
-JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.TRAILING));
-buttonPanel.setOpaque(false); // Set panel transparent
-buttonPanel.add(backButton);
-buttonPanel.add(logoutButton);
-     
+        // Tab for Deleting Customers
+        JPanel deleteCustomersPanel = new JPanel();
+        // Add components to deleteCustomersPanel...
+        tabbedPane.addTab("Delete Customer", null, deleteCustomersPanel, "Click to delete");
 
-       
+        // Add the tabbedPane to this panel
+        add(tabbedPane, BorderLayout.CENTER);
 
-      
-        
-        // Add the button panel to the top panel
-        topPanel.add(buttonPanel, BorderLayout.EAST);
-        topPanel.add(tabbedPane, BorderLayout.CENTER);
-
-        // Add the top panel to the frame
-        getContentPane().add(topPanel, BorderLayout.NORTH);
-    }
-
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                TabbedCustomerPanel frame = new TabbedCustomerPanel();
-                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                frame.setSize(900, 600); // Set the frame size
-                frame.setVisible(true); // Display the frame
-            }
+        // Top panel with buttons
+        JPanel topPanel = new JPanel(new FlowLayout(FlowLayout.TRAILING));
+        JButton backButton = ButtonBuilder.createBlueBackButton(() -> {
+            // Define the action for the 'Back' button
+            // Example: System.out.println("Back button clicked");
         });
+        JButton logoutButton = ButtonBuilder.createRedLogoutButton(() -> {
+            // Define the action for the 'Logout' button
+            // Example: System.out.println("Logout button clicked");
+        });
+
+        topPanel.add(backButton);
+        topPanel.add(logoutButton);
+
+        // Add the top panel to this panel
+        add(topPanel, BorderLayout.NORTH);
     }
 }
