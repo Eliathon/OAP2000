8a3fac0817866eb6f72e133822ccf0a84fe6cebf
Pat-Pas <Pastor301299@gmail.com>
2023-11-22 16:36:29 +0100

    .

--- app/src/main/java/com/oap200/app/views/OrderManagementPanel.java
+++ app/src/main/java/com/oap200/app/views/OrderManagementPanel.java
@@ -8,8 +8,9 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.util.prefs.Preferences;
 
+import com.oap200.app.tabbedPanels.TabbedCustomerPanel;
 import com.oap200.app.tabbedPanels.TabbedOrderPanel;
-import com.oap200.app.utils.ButtonBuilder;
+import com.oap200.utils.ButtonBuilder;
 
 public class OrderManagementPanel extends JFrame {
     
