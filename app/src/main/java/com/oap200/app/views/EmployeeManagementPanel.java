999789dbad0786337a07f6c4a0ff10170c499ced
Eliathon <sin-nov@online.no>
2023-11-23 16:58:11 +0100

    ENDA MER FIX

--- app/src/main/java/com/oap200/app/views/EmployeeManagementPanel.java
+++ app/src/main/java/com/oap200/app/views/EmployeeManagementPanel.java
@@ -5,25 +5,18 @@ import java.awt.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.util.prefs.Preferences;
-import com.oap200.app.utils.ButtonBuilder; // Ensure this class exists and functions as expected
+import com.oap200.app.utils.ButtonBuilder;
 
 public class EmployeeManagementPanel extends JFrame {
-    private JTextField employeeIdField;
-    private JTextField lastNameField;
-    private JTextField firstNameField;
-    private JTextField extensionField;
-    private JTextField emailField;
-    private JTextField officeCodeField;
-    private JTextField reportsToField;
-    private JTextField jobTitleField;
 
     private static final String PREF_X = "window_x";
     private static final String PREF_Y = "window_y";
 
     public EmployeeManagementPanel() {
+        initializeFields();
+
         // Load the last window position
-        Preferences prefs = Preferences.userNodeForPackage(EmployeeManagementPanel.class); // Ensure this refers to the
-                                                                                           // current class
+        Preferences prefs = Preferences.userNodeForPackage(EmployeeManagementPanel.class);
         int x = prefs.getInt(PREF_X, 50); // Default x position
         int y = prefs.getInt(PREF_Y, 50); // Default y position
         setLocation(x, y);
@@ -38,10 +31,6 @@ public class EmployeeManagementPanel extends JFrame {
 
         // Set up the layout for the frame
         setLayout(new BorderLayout());
-
-        // Initialize Panels
-        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.TRAILING));
-        JPanel topPanel = new JPanel(new BorderLayout());
 
         // Initialize ButtonBuilder buttons
         JButton backButton = ButtonBuilder.createBlueBackButton(() -> {
@@ -62,57 +51,32 @@ public class EmployeeManagementPanel extends JFrame {
 
         // Tab 1: View Employee
         JPanel panel1 = new JPanel(new BorderLayout());
-        panel1.add(viewButton, BorderLayout.CENTER);
+        addComponentsToPanelView(panel1);
+        panel1.add(viewButton, BorderLayout.SOUTH);
         tabbedPane.addTab("View Employee", null, panel1, "Click to view");
 
         // Tab 2: Add Employee
         JPanel panel2 = new JPanel(new BorderLayout());
-        JPanel labelPanel = new JPanel(new GridLayout(8, 1)); // 8 labels
-        JPanel fieldPanel = new JPanel(new GridLayout(8, 1)); // 8 fields
-
-        // Initialize and add fields and labels
-        employeeIdField = new JTextField(10);
-        lastNameField = new JTextField(10);
-        firstNameField = new JTextField(10);
-        extensionField = new JTextField(10);
-        emailField = new JTextField(10);
-        officeCodeField = new JTextField(10);
-        reportsToField = new JTextField(10);
-        jobTitleField = new JTextField(10);
-
-        labelPanel.add(new JLabel("Employee ID:"));
-        fieldPanel.add(employeeIdField);
-        labelPanel.add(new JLabel("Last Name:"));
-        fieldPanel.add(lastNameField);
-        labelPanel.add(new JLabel("First Name:"));
-        fieldPanel.add(firstNameField);
-        labelPanel.add(new JLabel("Extension:"));
-        fieldPanel.add(extensionField);
-        labelPanel.add(new JLabel("Email:"));
-        fieldPanel.add(emailField);
-        labelPanel.add(new JLabel("Office Code:"));
-        fieldPanel.add(officeCodeField);
-        labelPanel.add(new JLabel("Reports To:"));
-        fieldPanel.add(reportsToField);
-        labelPanel.add(new JLabel("Job Title:"));
-        fieldPanel.add(jobTitleField);
-
-        panel2.add(labelPanel, BorderLayout.WEST);
-        panel2.add(fieldPanel, BorderLayout.CENTER);
+        addComponentsToPanel(panel2);
         panel2.add(addButton, BorderLayout.SOUTH);
         tabbedPane.addTab("Add Employee", null, panel2, "Click to add");
 
         // Tab 3: Update Products
         JPanel panel3 = new JPanel(new BorderLayout());
-        panel3.add(updateButton, BorderLayout.CENTER);
+        addComponentsToPanel(panel3);
+        panel3.add(updateButton, BorderLayout.SOUTH);
         tabbedPane.addTab("Update Products", null, panel3, "Click to Update");
 
         // Tab 4: Delete Products
         JPanel panel4 = new JPanel(new BorderLayout());
-        panel4.add(deleteButton, BorderLayout.CENTER);
+        addComponentsToPanel(panel4);
+        panel4.add(deleteButton, BorderLayout.SOUTH);
         tabbedPane.addTab("Delete Products", null, panel4, "Click to Delete");
 
-        // Adding components to buttonPanel and topPanel
+        // Initialize Panels
+        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.TRAILING));
+        JPanel topPanel = new JPanel(new BorderLayout());
+
         buttonPanel.setOpaque(true);
         buttonPanel.add(backButton);
         buttonPanel.add(logoutButton);
@@ -123,12 +87,74 @@ public class EmployeeManagementPanel extends JFrame {
         getContentPane().add(topPanel, BorderLayout.NORTH);
     }
 
+    private void initializeFields() {
+        new JTextField(10);
+        new JTextField(10);
+        new JTextField(10);
+        new JTextField(10);
+        new JTextField(10);
+        new JTextField(10);
+        new JTextField(10);
+        new JTextField(10);
+    }
+
+    private void addComponentsToPanel(JPanel panel) {
+        JPanel labelPanel = new JPanel(new GridLayout(8, 1)); // 8 labels
+        JPanel fieldPanel = new JPanel(new GridLayout(8, 1)); // 8 fields
+
+        // Cloning fields for each tab
+        JTextField employeeId = new JTextField(10);
+        JTextField lastName = new JTextField(10);
+        JTextField firstName = new JTextField(10);
+        JTextField extension = new JTextField(10);
+        JTextField email = new JTextField(10);
+        JTextField officeCode = new JTextField(10);
+        JTextField reportsTo = new JTextField(10);
+        JTextField jobTitle = new JTextField(10);
+
+        labelPanel.add(new JLabel("Employee ID:"));
+        fieldPanel.add(employeeId);
+        labelPanel.add(new JLabel("Last Name:"));
+        fieldPanel.add(lastName);
+        labelPanel.add(new JLabel("First Name:"));
+        fieldPanel.add(firstName);
+        labelPanel.add(new JLabel("Extension:"));
+        fieldPanel.add(extension);
+        labelPanel.add(new JLabel("Email:"));
+        fieldPanel.add(email);
+        labelPanel.add(new JLabel("Office Code:"));
+        fieldPanel.add(officeCode);
+        labelPanel.add(new JLabel("Reports To:"));
+        fieldPanel.add(reportsTo);
+        labelPanel.add(new JLabel("Job Title:"));
+        fieldPanel.add(jobTitle);
+
+        panel.add(labelPanel, BorderLayout.WEST);
+        panel.add(fieldPanel, BorderLayout.CENTER);
+    }
+
+    private void addComponentsToPanelView(JPanel panelView) {
+        JPanel labelPanel = new JPanel(new GridLayout(2, 1)); // 8 labels
+        JPanel fieldPanel = new JPanel(new GridLayout(2, 1)); // 8 fields
+
+        JTextField employeeId = new JTextField(10);
+        JTextField lastName = new JTextField(10);
+
+        labelPanel.add(new JLabel("Employee ID:"));
+        fieldPanel.add(employeeId);
+        labelPanel.add(new JLabel("Last Name:"));
+        fieldPanel.add(lastName);
+
+        panelView.add(labelPanel, BorderLayout.WEST);
+        panelView.add(fieldPanel, BorderLayout.CENTER);
+    }
+
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             EmployeeManagementPanel frame = new EmployeeManagementPanel();
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             frame.setTitle("Employee Management");
-            frame.pack(); // Adjust to fit components
+            frame.pack();
             frame.setVisible(true);
         });
     }
