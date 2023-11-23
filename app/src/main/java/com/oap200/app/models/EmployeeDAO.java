489871f82f7ad579c3c0ff469db1961c8e5699f4
Eliathon <sin-nov@online.no>
2023-11-23 18:58:43 +0100

    MISC

--- app/src/main/java/com/oap200/app/models/EmployeeDAO.java
+++ app/src/main/java/com/oap200/app/models/EmployeeDAO.java
@@ -1,5 +1,41 @@
 package com.oap200.app.models;
 
+import com.oap200.app.utils.DbConnect;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class EmployeeDAO {
-    
+
+    public List<String[]> fetchEmployees() {
+        List<String[]> employees = new ArrayList<>();
+
+        try {
+            DbConnect db = new DbConnect();
+            Connection myConnection = db.getConnection();
+            Statement myStmt = myConnection.createStatement();
+            ResultSet myRs = myStmt.executeQuery("SELECT * FROM employees");
+
+            while (myRs.next()) {
+                String[] employee = new String[] {
+                        myRs.getString("employeeNumber"),
+                        myRs.getString("lastName"),
+                        myRs.getString("firstName"),
+                        myRs.getString("extension"),
+                        myRs.getString("email"),
+                        myRs.getString("officeCode"),
+                        myRs.getString("reportsTo"),
+                        myRs.getString("jobTitle"),
+                };
+                employees.add(employee);
+            }
+        } catch (SQLException | ClassNotFoundException ex) {
+            ex.printStackTrace();
+        }
+        return employees;
+    }
 }
