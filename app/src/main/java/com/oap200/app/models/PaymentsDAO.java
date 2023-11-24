256e354823cd58fd41fdba68911711d2c536c33c
LorexKrato <lorexkrato@gmail.com>
2023-11-24 20:43:04 +0100

    jfjf

--- app/src/main/java/com/oap200/app/models/PaymentsDAO.java
+++ app/src/main/java/com/oap200/app/models/PaymentsDAO.java
@@ -65,5 +65,23 @@ public class PaymentsDAO {
         }
         return true;
     }
-    
+    public boolean updatePayments(String customerNumber, String newCheckNumber, String newPaymentDate, String newAmount) {
+        String updateSql = "UPDATE payments SET checkNumber = ?, paymentDate = ?, amount = ? WHERE customerNumber = ?";
+
+        try (Connection conn = new DbConnect().getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(updateSql)) {
+
+            pstmt.setString(1, newCheckNumber);
+            pstmt.setString(2, newPaymentDate);
+            pstmt.setString(3, newAmount);
+            pstmt.setString(4, customerNumber);
+
+            int rowsAffected = pstmt.executeUpdate();
+
+            return rowsAffected > 0;  // Return true if at least one row is affected (update successful)
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 }
