4689b03f6d9347263afe77e6307518d349929a11
LorexKrato <lorexkrato@gmail.com>
2023-11-25 17:49:02 +0100

    more

--- app/src/main/java/com/oap200/app/views/PaymentManagement.java
+++ app/src/main/java/com/oap200/app/views/PaymentManagement.java
@@ -197,47 +197,53 @@ private void viewAmount() {
    
 
     private void addComponentsToPanelView(JPanel panelView) {
-        panelView.setLayout(new BorderLayout());
-    
-        JPanel inputPanel = new JPanel(new GridBagLayout());
-        GridBagConstraints gbc = new GridBagConstraints();
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-        gbc.weightx = 0.3; // Button weight
-    
-        // Adding the "Customer Number" sorting button
-        JButton sortCustomerButton = new JButton("Sort by Customer Number");
-        sortCustomerButton.addActionListener(e -> viewSortedData("customerNumber"));
-        inputPanel.add(sortCustomerButton, gbc);
-    
-        // Adding the "Check Number" sorting button
-        JButton sortCheckButton = new JButton("Sort by Check Number");
-        sortCheckButton.addActionListener(e -> viewSortedData("checkNumber"));
-        gbc.gridx = 1;
-        inputPanel.add(sortCheckButton, gbc);
-    
-        // Adding the "Payment Date" sorting button
-        JButton sortDateButton = new JButton("Sort by Payment Date");
-        sortDateButton.addActionListener(e -> viewSortedData("paymentDate"));
-        gbc.gridx = 2;
-        inputPanel.add(sortDateButton, gbc);
-    
-        // Adding the "Amount" sorting button
-        JButton sortAmountButton = new JButton("Sort by Amount");
-        sortAmountButton.addActionListener(e -> viewSortedData("amount"));
-        gbc.gridx = 3;
-        inputPanel.add(sortAmountButton, gbc);
-    
-        panelView.add(inputPanel, BorderLayout.NORTH);
-    
-        // Now, create the scrollPane with the paymentsTable right here:
-        JScrollPane scrollPane = new JScrollPane(paymentsTable);
-    
-        // Create a container panel for the table to align it to the left
-        JPanel tableContainer = new JPanel(new BorderLayout());
-        tableContainer.add(scrollPane, BorderLayout.CENTER);
-        panelView.add(tableContainer, BorderLayout.CENTER);
-    }
-    
+    panelView.setLayout(new BorderLayout());
+
+    JPanel inputPanel = new JPanel(new GridBagLayout());
+    GridBagConstraints gbc = new GridBagConstraints();
+    gbc.fill = GridBagConstraints.HORIZONTAL;
+    gbc.weightx = 0.3; // Button weight
+
+    // Adding the "Customer Number" sorting button
+    JButton sortCustomerButton = new JButton("Sort by Customer Number");
+    sortCustomerButton.addActionListener(e -> viewSortedData("customerNumber"));
+    inputPanel.add(sortCustomerButton, gbc);
+
+    // Adding the "Check Number" sorting button
+    JButton sortCheckButton = new JButton("Sort by Check Number");
+    sortCheckButton.addActionListener(e -> viewSortedData("checkNumber"));
+    gbc.gridx = 1;
+    inputPanel.add(sortCheckButton, gbc);
+
+    // Adding the "Payment Date" sorting button
+    JButton sortDateButton = new JButton("Sort by Payment Date");
+    sortDateButton.addActionListener(e -> viewSortedData("paymentDate"));
+    gbc.gridx = 2;
+    inputPanel.add(sortDateButton, gbc);
+
+    // Adding the "Amount" sorting button
+    JButton sortAmountButton = new JButton("Sort by Amount");
+    sortAmountButton.addActionListener(e -> viewSortedData("amount"));
+    gbc.gridx = 3;
+    inputPanel.add(sortAmountButton, gbc);
+
+    // Adjusting button size based on header size
+    Dimension buttonSize = sortCustomerButton.getPreferredSize();
+    sortCheckButton.setPreferredSize(buttonSize);
+    sortDateButton.setPreferredSize(buttonSize);
+    sortAmountButton.setPreferredSize(buttonSize);
+
+    panelView.add(inputPanel, BorderLayout.NORTH);
+
+    // Now, create the scrollPane with the paymentsTable right here:
+    JScrollPane scrollPane = new JScrollPane(paymentsTable);
+
+    // Create a container panel for the table to align it to the left
+    JPanel tableContainer = new JPanel(new BorderLayout());
+    tableContainer.add(scrollPane, BorderLayout.CENTER);
+    panelView.add(tableContainer, BorderLayout.CENTER);
+}
+
     
 
     public static void main(String[] args) {
